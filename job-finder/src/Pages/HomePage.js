34716553629222b04eaa7db9428d4e5be24999de
Güven Fazlı <guven.fazli@gmail.com>
2024-03-30 14:12:04 +0300

    Company logo section is done

--- job-finder/src/Pages/HomePage.js
+++ job-finder/src/Pages/HomePage.js
@@ -1,12 +1,13 @@
 import Header from "../Components/Header/Header"
 import Home from "../Components/Home/Home"
-
+import Company from "../Components/Companies/Company"
 
 export default function HomePage(){
   return (
     <>
       <Header />
       <Home />
+      <Company />
     
     
     </>
