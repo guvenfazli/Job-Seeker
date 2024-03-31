9af011fb9b66e294b2e8c6173dbb8feb0851f31c
Güven Fazlı <guven.fazli@gmail.com>
2024-03-31 18:12:09 +0300

    Empty Company Pictures set

--- job-finder/src/Components/AvlJobs/JobCard/JobCard.jsx
+++ job-finder/src/Components/AvlJobs/JobCard/JobCard.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom"
 import { calculateDate } from "../../../Utils/fetchData"
 import classes from "./jobcard.module.css"
 import calendar from "../../../Assets/JobCardAssets/Calendar.png"
+import noCompany from "../../../Assets/JobListAssets/None.png"
+
 
 export default function JobCard({ job }) {
 
@@ -13,9 +15,8 @@ export default function JobCard({ job }) {
     <div className={classes.card}>
       <div className={classes.companyInfo}>
         <div className={classes.primaryInfo}>
-          <div className={classes.companyImg} style={{backgroundImage: `url(${companyImage})`}}>
-            {!companyImage && "No Img"}
-          </div>
+          <div className={classes.companyImg} style={{backgroundImage: `url(${companyImage ? companyImage : noCompany})`}} />
+          
           <div className={classes.companyTitle}>
             <p>{job.employer_name}</p>
             <p>{job.job_city} {job.job_city && ","} {job.job_country}</p>
--- job-finder/src/Components/JobList/Job/Job.jsx
+++ job-finder/src/Components/JobList/Job/Job.jsx
@@ -1,33 +1,33 @@
 import classes from "./job.module.css"
 import { calculateDate } from "../../../Utils/fetchData"
-
-export default function Job({job}){
+import noCompany from "../../../Assets/JobListAssets/None.png"
+export default function Job({ job }) {
   const postedDaysAgo = calculateDate(job.job_posted_at_datetime_utc)
   const companyImage = job.employer_logo
   console.log(companyImage)
   return (
     <div className={classes.job}>
       <div className={classes.jobLeft}>
-        <div className={classes.jobImg} style={{backgroundImage: `url(${companyImage})`}}>
-          {companyImage === null && "No Image"}
-        </div>
+        <div className={classes.jobImg} style={{ backgroundImage: `url(${companyImage ? companyImage : noCompany})` }} />
+
+
 
         <div className={classes.jobDetails}>
-          <p style={{marginTop: "0px", fontWeight: "bold", fontSize: "16px"}}>{job.job_title}</p>
+          <p style={{ marginTop: "0px", fontWeight: "bold", fontSize: "16px" }}>{job.job_title}</p>
           <p>{job.employer_name}</p>
-          <p style={{color: "gray"}}>{job.job_city} {job.job_city && ","} {job.job_country}</p>
-          <p style={{color: "gray"}}>{job.job_is_remote ? "Remote" : "On-Site"}</p>
-          <p style={{marginBottom: "0px", fontSize: "12px"}}>{postedDaysAgo === 0 ? "Recently posted" : `Posted ${postedDaysAgo} days ago`}</p>
+          <p style={{ color: "gray" }}>{job.job_city} {job.job_city && ","} {job.job_country}</p>
+          <p style={{ color: "gray" }}>{job.job_is_remote ? "Remote" : "On-Site"}</p>
+          <p style={{ marginBottom: "0px", fontSize: "12px" }}>{postedDaysAgo === 0 ? "Recently posted" : `Posted ${postedDaysAgo} days ago`}</p>
         </div>
       </div>
 
       <div className={classes.jobRight}>
-      
-          <button>Apply</button>
 
-        
+        <button>Apply</button>
+
+
       </div>
-    
+
     </div>
   )
 }
